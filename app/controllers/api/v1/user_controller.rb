b8bbb3097e71cd59338e18a0208371c81ba0e177
basantrijal <rijalbsn@gmail.com>
2015-06-23 16:10:46 +0545

    user registration added

---
+++ app/controllers/api/v1/user_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::UserController  < ApplicationController
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render :json => user.as_json(:email=>user.email), :status=>201
+      return
+    else
+      warden.custom_failure!
+      render :json => user.errors, :status=>422
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
