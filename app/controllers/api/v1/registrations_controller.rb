42832835b001b9a0a6f466803e58bd3477aa4bbc
basantrijal <rijalbsn@gmail.com>
2015-06-23 17:28:53 +0545

    pic upload added

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -3,6 +3,8 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
 
   def create
     user = User.new(user_params)
+    avatar_uploaded_path = upload_profile_pic(params[:user][:profile_pic])
+    user.profile_pic = avatar_uploaded_path
     if user.save
       render :json => user.as_json(:email => user.email), :status => 201
       return
@@ -12,8 +14,14 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
     end
   end
 
+  def upload_profile_pic(avatar_path)
+    filename = avatar_path.split('/')
+    FileUtils.cp_r avatar_path, Rails.public_path
+    return request.base_url+'/'+filename[-1]
+  end
+
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :profile_pic)
   end
 
 end
