8debc484507ee4e989dbf5a7bc519fb42ff6b79b
basantrijal <rijalbsn@gmail.com>
2015-06-26 13:38:11 +0545

    adding user controller

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,5 @@
+class ApiController < ApplicationController
+	# No action on this controller is accessible without a
+    # supplying a valid token.
+	before_filter :verify_jwt_token
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,15 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      devise_for :users, :stateless_token => true, controllers: {
-                           registrations: "api/v1/registrations",
-                           sessions: 'api/v1/login'
-                       }
+      devise_for :user,
+             :path => "/",
+             :path_names => {:sign_in => 'login', :registrations => 'register'},
+      			 :stateless_token => true,
+      			 controllers: { 
+                  	registrations: "api/v1/registrations",
+                    sessions: 'api/v1/login',
+                  }
+      resource 'users'
     end
   end
 
