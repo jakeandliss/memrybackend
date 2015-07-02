fbce555f4d4b9b313d92b584045ce18f6ab278ae
basantrijal <rijalbsn@gmail.com>
2015-07-03 00:32:06 +0545

    old remove login and registrations controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,8 @@
 module Api
   module V1
     class UsersController  < ApplicationController
-
+      skip_before_action :verify_authenticity_token, if: :json_request?
+      skip_before_filter  :verify_authenticity_token
     def create
       user = User.new(user_params)
       if user.save
