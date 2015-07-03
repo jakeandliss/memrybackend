3cbe587596b4b12f675acfed3d80211ee073f844
basantrijal <rijalbsn@gmail.com>
2015-07-03 15:21:57 +0545

    create method used

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,8 +3,9 @@ module Api
     class UsersController  < ApplicationController
       skip_before_action :verify_authenticity_token, if: :json_request?
       skip_before_filter  :verify_authenticity_token
+      
     def create
-      user = User.new(user_params)
+      user = User.create(user_params)
       if user.save
         render json: { message: 'Thanks for signing up!' }, status: :created
       else
