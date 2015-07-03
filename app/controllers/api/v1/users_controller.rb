b63845bc67bbfb9faeffcad2c5444c3e63e28efd
basantrijal <rijalbsn@gmail.com>
2015-07-03 13:56:11 +0545

    added user update action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,21 +3,30 @@ module Api
     class UsersController  < ApplicationController
       skip_before_action :verify_authenticity_token, if: :json_request?
       skip_before_filter  :verify_authenticity_token
-    def create
-      user = User.new(user_params)
-      if user.save
-        render json: { message: 'Thanks for signing up!' }, status: :created
-      else
-        render json: { error: user.errors }, status: :not_acceptable
+
+      def create
+        user = User.new(user_params)
+        if user.save
+          render json: { message: 'Thanks for signing up!' }, status: :created
+        else
+          render json: { error: user.errors }, status: :not_acceptable
+        end
       end
-    end
 
-    def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :avatar)
-    end
+      def update
+        user = User.find(params[:id])
+        if user.update_attributes(user_params)
+          render json: { message: 'Your profile has been updated' }, status: :ok
+        else
+          render json: { error: user.errors }, status: :not_acceptable
+        end
+      end
+
+
+      def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :avatar)
+      end
 
     end
-
   end
-
 end
