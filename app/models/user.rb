15fd3fce3bc7801d113f5cf9b2579ec0e3d6ec1b
basantrijal <rijalbsn@gmail.com>
2015-07-04 00:20:13 +0545

    validation of attribute presence as a whole

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,6 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :medium => "300x300>", :avatar =>  "150x150#", :thumb => "100x100>" }, :default_url => "blank_avatar.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/, reject_if: :blank
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
+  validates :first_name, :last_name, :email, :password, presence: true
 
 end
