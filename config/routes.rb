c969d8227e0a6e5b8360c6fa4ab30090b7f5856c
basantrijal <rijalbsn@gmail.com>
2015-07-07 22:38:27 +0545

    route collection cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
 
   namespace 'api' do
     namespace 'v1', :constraints => {format: 'json'} do
-      resources :registrations, :controller => "users", :only => [:create]
       resources :users, :controller => "users", :only => [:update, :show, :destroy] do
-        collection { post 'check_user', :to => "users#check_user_exists"  }
+        collection  do
+          post 'check_user', :to => "users#check_user_exists"
+        end
       end
       resources :forgot_password, :to => "users#forgot_password", :only => [:create]
       resources :change_password, :to => "users#change_password", :only => [:show, :update]
