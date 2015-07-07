b8cc2b426866bd3996e916234eeff0a5fa5d25b3
basantrijal <rijalbsn@gmail.com>
2015-07-07 13:40:44 +0545

    route fix

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,12 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1', :constraints => {format: 'json'} do
       resources :registrations, :controller => "users", :only => [:create]
-      resources :users, :controller => "users", :only => [:update, :show, :destroy]
+      resources :users, :controller => "users", :only => [:update, :show, :destroy] do
+        collection { post 'check_user', :to => "users#check_user_exists"  }
+      end
       resources :forgot_password, :to => "users#forgot_password", :only => [:create]
       resources :change_password, :to => "users#change_password", :only => [:show, :update]
-      resources :check_user, :to => "users#check_user_exists", :only => [:create]
+      # resources :check_user, :to => "users#check_user_exists", :only => [:create]
     end
   end
 
