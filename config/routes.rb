b307fc017793b3b5d0ab24820f2a6da976571cf1
basantrijal <rijalbsn@gmail.com>
2015-07-07 22:22:26 +0545

    default route for user registrations

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
 
   namespace 'api' do
     namespace 'v1', :constraints => {format: 'json'} do
-      resources :users, :controller => "users", :only => [:update]
-      match 'registrations' => 'users#create', via: :post
+      resources :users, :controller => "users", :only => [:create, :update]
     end
   end
 end
