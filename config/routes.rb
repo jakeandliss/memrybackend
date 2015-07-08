9deebaa67a78d75c4ae839a7461e8732169aa9fe
basantrijal <rijalbsn@gmail.com>
2015-07-08 13:51:25 +0545

    merge conflict fix

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
 
   namespace 'api' do
     namespace 'v1', :constraints => {format: 'json'} do
-      resources :registrations, :controller => "users", :only => [:create]
       resources :users, :controller => "users", :only => [:update, :show, :destroy]
     end
 
