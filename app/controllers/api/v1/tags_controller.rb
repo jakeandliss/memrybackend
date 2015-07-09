8afeef685ed86dc22fa5c94361e1b4ef2bf7ce19
basantrijal <rijalbsn@gmail.com>
2015-07-09 15:28:00 +0545

    tag with children deletion added

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::TagsController < ApplicationController
   skip_before_action :verify_authenticity_token, if: :json_request?
   skip_before_filter  :verify_authenticity_token
-  before_action :ancestor_tag, only: [:create]
+  before_action :ancestor_tag, only: [:create, :destroy]
 
   def create
-    @tag = Tag.create!(name: params[:tag][:name], user_id: 22, ancestry: @ancestor_tag)
+    @tag = Tag.create!(name: params[:name], user_id: 22, ancestry: @ancestor_tag.id)
     if @tag.save
       render json: { message: "New tag created" }, status: :created
     else
@@ -12,13 +12,17 @@ class Api::V1::TagsController < ApplicationController
     end
   end
 
+  def destroy
+    @ancestor_tag.destroy
+    render json: { message: "Tag deleted successfully" }, status: :ok
+  end
+
 
   private
   def ancestor_tag
     @ancestor_tag = nil;
-    if params[:tag][:parent]
-      tag = Tag.find(params[:tag][:parent])
-      @ancestor_tag = tag.id
+    if params[:id]
+      @ancestor_tag = Tag.find(params[:id])
     end
   end
 
