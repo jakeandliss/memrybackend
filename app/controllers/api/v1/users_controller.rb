9d7ed5561ead41c755c424ba8b144fb2820576fe
Allan Xavier <allanxavierm@gmail.com>
2015-07-11 12:47:21 +0530

    adding method to validate the json schema posted from the clients

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,23 +2,33 @@ module Api
   module V1
     class UsersController  < ApplicationController
       skip_before_action :verify_authenticity_token, if: :json_request?
-      skip_before_filter  :verify_authenticity_token
+      skip_before_filter :verify_authenticity_token
+      before_action      :validate_schema, only: [:create]
       
     def create
-      user = User.create(user_params)
+      user = User.new(@registration_attr)
+
       if user.save
         render json: { message: 'Thanks for signing up!' }, status: :created
       else
-        render json: { error: user.errors }, status: :not_acceptable
+        report_errors_on(user)
       end
     end
 
-    def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :avatar)
-    end
+    private
 
-    end
+      def validate_schema
+        validate_json('userRegistration', params.require(:userRegistration))
+        if @errors.empty?
+          registration_attributes
+        else
+          render json: {message: @errors}, status: 422
+        end
+      end
 
+      def registration_attributes
+        @registration_attr = convert_hash_keys(params.require(:userRegistration).permit(:firstName, :lastName, :email, :password))
+      end
+    end
   end
-
 end
