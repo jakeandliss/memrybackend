30fb0e37a49e294042d531365ba6c9344a586362
Allan Xavier <allanxavierm@gmail.com>
2015-09-03 17:22:16 +0000

    adding http status symbols and localizing error messages

--- app/controllers/api/v1/entries_controller.rb
+++ app/controllers/api/v1/entries_controller.rb
@@ -11,13 +11,13 @@ module Api
 
       def show
         @entry = @user.entries.where(id: params[:id] ).first
-        render json: "Entry Not found", status: 422 if @entry.blank?
+        render json: "Entry Not found", status: :not_found if @entry.blank?
       end
 
       def create
         @entry = @user.entries.build(entry_params)
         unless @entry.save
-          render json: {error: @entry.errors.full_messages.join(', ')}, status: 422
+          render json: {error: @entry.errors.full_messages.join(', ')}, status: :unprocessable_entity
         end
       end
 
@@ -25,10 +25,10 @@ module Api
         @entry = @user.entries.where(id: params[:id] ).first
         if @entry.present?
           unless @entry.update_attributes(entry_params)
-            render json: {error: @entry.errors.full_messages.join(', ')}, status: 422
+            render json: {error: @entry.errors.full_messages.join(', ')}, status: :unprocessable_entity
           end
         else
-          render json: "Entry Not found", status: 422
+          render json: "Entry Not found", status: :not_found
         end
       end
 
@@ -36,12 +36,12 @@ module Api
         @entry = @user.entries.where(id: params[:id] ).first
         if @entry.present?
           if @entry.destroy
-            render json: 'Entry deleted successfully', status: 200 
+            render json: 'Entry deleted successfully', status: :ok
           else
-            render json: 'Sorry! Entry cannot be deleted now', status: 422
+            render json: 'Sorry! Entry cannot be deleted now', status: :unprocessable_entity
           end
         else
-          render json: "Entry Not found", status: 422 
+          render json: "Entry Not found", status: :not_found
         end
       end
 
@@ -49,8 +49,10 @@ module Api
 
       def set_user
         @user = User.find_by_id(params[:user_id])
-        render json: 'User Not Found', status: 422 if @user.blank?
+        render json: 'User Not Found', status: :not_found if @user.blank?
       end
+
+      def
 
       def entry_params
         params.require(:entry).permit(:title, :content, resources_attributes: [:attachment] )
